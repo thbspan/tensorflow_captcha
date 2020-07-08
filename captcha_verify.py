5a336c92e2538f7795b0be20ac71b9574f2818a0
thbspan <thbspan@163.com>
2020-07-08 21:33:18 +0800

    init project

---
+++ captcha_verify.py
@@ -0,0 +1,35 @@
+import random
+
+from captcha.image import ImageCaptcha
+
+import config
+
+
+def random_captcha_text(char_set=None, captcha_size=4):
+    if char_set is None:
+        char_set = config.NUMBER
+    captcha_text = []
+    for i in range(captcha_size):
+        c = random.choice(char_set)
+        captcha_text.append(c)
+    return captcha_text
+
+
+# 生成字符对应的验证码
+def gen_captcha_text_and_image():
+    image = ImageCaptcha(width=config.IMAGE_WIDTH, height=config.IMAGE_HEIGHT, font_sizes=[config.FONT_SIZE])
+    # 获得随机生成的验证码
+    captcha_text = random_captcha_text()
+    # 把验证码列表转为字符串
+    captcha_text = ''.join(captcha_text)
+    # 生成验证码
+    image.generate(captcha_text)
+    # 写到文件
+    image.write(captcha_text, 'images/' + captcha_text + '.jpg')
+
+
+if __name__ == '__main__':
+    num = 10000
+    for i in range(num):
+        gen_captcha_text_and_image()
+    print("生成完毕")
---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+CHAR_SMALL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+              'v', 'w', 'x', 'y', 'z']
+CHAR_BIG = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+
+VALIDATE_CHAR = NUMBER + CHAR_SMALL + CHAR_BIG
+CHAR_SET_LEN = len(VALIDATE_CHAR)
+
+IMAGE_HEIGHT = 60
+IMAGE_WIDTH = 160
+FONT_SIZE = 35
